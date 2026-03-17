ac314d3803ed3c0c4ce80e7edfaa42821fff6430
Xiomara <Xiomara.perez@udea.edu.co>
2026-03-16 22:08:09 -0500

    Actualiza backend: cambios recientes

--- src/main/java/com/udea/banco2026v/mapper/CustomerMapper.java
+++ src/main/java/com/udea/banco2026v/mapper/CustomerMapper.java
@@ -3,11 +3,9 @@ package com.udea.banco2026v.mapper;
 import com.udea.banco2026v.dto.CustomerDTO;
 import com.udea.banco2026v.entity.Customer;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
-    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
 
     CustomerDTO toDTO(Customer customer);
 
