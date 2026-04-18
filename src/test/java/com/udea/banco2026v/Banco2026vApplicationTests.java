43b51308bd2c8866b0aae49625748acea5ce3044
Xiomara <Xiomara.perez@udea.edu.co>
2026-04-17 22:56:41 -0500

    Config perfiles dev/test + H2 + tests OK

--- src/test/java/com/udea/banco2026v/Banco2026vApplicationTests.java
+++ src/test/java/com/udea/banco2026v/Banco2026vApplicationTests.java
@@ -2,8 +2,10 @@ package com.udea.banco2026v;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Banco2026vApplicationTests {
 
 	@Test
