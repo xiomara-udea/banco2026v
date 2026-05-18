c87e1842ea871e2905432746021d84668138570b
Xiomara <Xiomara.perez@udea.edu.co>
2026-05-17 20:26:55 -0500

    Add controller

---
+++ src/main/java/com/udea/banco2026v/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.udea.banco2026v.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "BANCO2026V RUNNING OK";
+    }
+}
